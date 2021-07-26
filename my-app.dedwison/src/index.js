fa9ef573ff0fa459dae157138fd0054ce05c7af6
Dedwison Alvarez <dedwison@gmail.com>
2021-07-25 21:46:07 -0400

    cree mi repositorio

---
+++ my-app.dedwison/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>PRIMER DESAFIO</h1>
+    <h3>clase dos</h3>
+  </div>,
+  document.getElementById("root")
+);
